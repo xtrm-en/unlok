ffb0b9440736b74571eba0c6f6b99be3299d9cb6
xtrm-en <oss@xtrm.me>
2022-02-19 22:12:14 +0100

    main: refactored a bit test: added final field test

---
+++ src/test/kotlin/methods.kt
@@ -0,0 +1,16 @@
+internal class MethodAccessorTests {
+
+    /* This is still testing syntax
+    @Test
+    fun `can call private method`() {
+        method(PrivateMethodHolder::class.java, "getPrivateStringy", Type.getType("java/lang/String"))
+            .invoke<String>()
+    }
+     */
+
+}
+
+internal class PrivateMethodHolder {
+    private fun getPrivateStringy(): String =
+        "real private stuff"
+}
