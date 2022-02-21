f06919a9b24bfcb95f30244572efd22f2e0a4ff0
xtrm-en <26600206+xtrm-en@users.noreply.github.com>
2022-02-21 03:52:54 +0100

    main: removed debug outputs

--- src/main/kotlin/me/xtrm/unlok/accessor/AccessorClassLoader.kt
+++ src/main/kotlin/me/xtrm/unlok/accessor/AccessorClassLoader.kt
@@ -3,8 +3,6 @@ package me.xtrm.unlok.accessor
 import me.xtrm.unlok.Unlok
 import org.objectweb.asm.ClassWriter
 import org.objectweb.asm.tree.ClassNode
-import java.io.File
-import java.io.FileOutputStream
 
 /**
  * @author xtrm-en
@@ -24,9 +22,6 @@ object AccessorClassLoader : ClassLoader(Unlok::class.java.classLoader) {
     fun load(classNode: ClassNode, cwFlags: Int = ClassWriter.COMPUTE_FRAMES): Class<*> {
         val className = classNode.name
         val bytecode = ClassWriter(cwFlags).also(classNode::accept).toByteArray()
-
-        val file = File("out/${className}.class").also(File::delete).also { it.parentFile.mkdirs() }
-        FileOutputStream(file).also { it.write(bytecode) }.close()
 
         return this.defineClass(className.replace('/', '.'), bytecode, 0, bytecode.size)
     }
