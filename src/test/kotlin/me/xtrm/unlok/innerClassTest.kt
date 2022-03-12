322f7ab1bf9c0616a630cffbddbaa553a2993988
lambdagg <oss@xtrm.me>
2022-03-12 10:34:27 +0100

    test: added inner class tests

---
+++ src/test/kotlin/me/xtrm/unlok/innerClassTest.kt
@@ -0,0 +1,34 @@
+package me.xtrm.unlok
+
+import me.xtrm.unlok.dsl.field
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class InnerClassTest {
+    @Test
+    fun `can access inner classes static fields`() {
+        val privateStaticFinalField by ParentClass.InnerClass::class.field<Int>()
+        assertEquals(69, privateStaticFinalField)
+    }
+
+    @Test
+    fun `can access inner classes virtual fields`() {
+        val innerClass = ParentClass.InnerClass()
+        val privateFinalField by field<Int>(
+            "me.xtrm.unlok.ParentClass.InnerClass",
+            ownerInstance = innerClass
+        )
+        assertEquals(42, privateFinalField)
+    }
+}
+
+class ParentClass {
+    class InnerClass {
+        companion object {
+            @JvmStatic
+            private val privateStaticFinalField = 69
+        }
+
+        private val privateFinalField = 42
+    }
+}
