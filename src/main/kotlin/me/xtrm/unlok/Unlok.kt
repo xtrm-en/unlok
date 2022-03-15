e73aeed8e7c0e1333988295335ae1252865bc8ea
xtrm-en <oss@xtrm.me>
2022-03-15 15:15:52 +0100

    main: fix deencapsulation flow

--- src/main/kotlin/me/xtrm/unlok/Unlok.kt
+++ src/main/kotlin/me/xtrm/unlok/Unlok.kt
@@ -1,5 +1,6 @@
 package me.xtrm.unlok
 
+import dev.xdark.deencapsulation.Deencapsulation
 import me.xtrm.unlok.api.accessor.FieldAccessor
 import me.xtrm.unlok.delegate.FieldDelegate
 import me.xtrm.unlok.delegate.MethodDelegate
@@ -15,6 +16,11 @@ object Unlok {
     init {
         if (!supportsCurrentVM()) {
             throw RuntimeException("Current JVM is not supported by Unlok.")
+        }
+
+        try {
+            Deencapsulation.deencapsulate(Class::class.java)
+        } catch (_: Throwable) {
         }
     }
 
